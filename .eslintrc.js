1619ae50e22179dcdafdf68e4ebe26d23bc662e1
0xsj <sjtommylee@gmail.com>
2025-03-05 23:30:51 -0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended'
+    ],
+    plugins: ['@typescript-eslint'],
+    env: {
+      node: true,
+      jest: true
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  };
